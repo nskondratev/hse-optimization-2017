e8bc5eaebaa162e6a4049e65a09a66910999659f
Nikita Kondratev <highflyer16@yandex.ru>
2017-09-20 23:43:54 +0300

    Feature. Solve small instance with 2-opt algorithm.

--- lab_1/main.py
+++ lab_1/main.py
@@ -1,10 +1,10 @@
 import lab_1.util as util
+import lab_1.algorithms as alg
 
 if __name__ == '__main__':
     test_filename = 'test3'
     n, d, f = util.read_from_file(test_filename)
     print('Read from file: n = {}, d.shape = {}, f.shape = {}'.format(n, d.shape, f.shape))
-    sol = util.build_initial_solution(n)
-    sol_obj = util.calc_obj_fun_value(sol, n, d, f)
-    print('Find initial solution: {}, objective func value = {}'.format(sol, sol_obj))
+    sol, sol_obj = alg.two_opt(n, d, f)
+    print('Find solution: {}, objective func value = {}'.format(sol, sol_obj))
 
