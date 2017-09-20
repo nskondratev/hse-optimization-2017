17f7022af75430834b46b436c5bc88ac3c7286cc
Nikita Kondratev <highflyer16@yandex.ru>
2017-09-20 23:23:56 +0300

    Feature. Implement 2-opt algorithm.

--- lab_1/util.py
+++ lab_1/util.py
@@ -4,6 +4,7 @@ import re
 
 TEST_INSTANCES_DIR = 'test_instances'
 
+
 # Helper function to parse one matrix from file
 def read_n_lines_as_matrix(f, n):
     res = ''
@@ -17,6 +18,7 @@ def read_n_lines_as_matrix(f, n):
     res[res == 0] = np.nan
     return res
 
+
 # Read file and return number of facilities and locations, flows and distance matrices
 def read_from_file(filename):
     with open(os.path.join(TEST_INSTANCES_DIR, filename)) as f:
@@ -25,6 +27,7 @@ def read_from_file(filename):
         f.readline()
         fm = read_n_lines_as_matrix(f, n)
         return n, dm, fm
+
 
 # Return objective function value based on provided solution, n, distance and flows matrices
 def calc_obj_fun_value(solution, n, d, f):
@@ -38,3 +41,31 @@ def calc_obj_fun_value(solution, n, d, f):
 # Build random initial solution
 def build_initial_solution(n):
     return np.random.permutation(n)
+
+
+# 2-opt algorithm
+def two_opt(n, d, f):
+    # Step 1
+    prev_best_solution = build_initial_solution(n)
+    while True:
+        # Step 2
+        current_best_solution = prev_best_solution.copy()
+        current_best_obj_value = calc_obj_fun_value(current_best_solution, n, d, f)
+        current_solution = current_best_solution.copy()
+        for i in range(n):
+            for j in range(i + 1, n):
+                # Change i facility with j
+                temp = current_solution[i]
+                current_solution[i] = current_solution[j]
+                current_solution[j] = temp
+                cur_obj_value = calc_obj_fun_value(current_solution, n, d, f)
+                # Compare with current best result
+                if cur_obj_value < current_best_obj_value:
+                    current_best_solution = current_solution.copy()
+                    current_best_obj_value = cur_obj_value
+        # Step 3
+        if np.array_equal(current_best_solution, prev_best_solution):
+            break
+        else:
+            prev_best_solution = current_best_solution
+    return prev_best_solution, calc_obj_fun_value(prev_best_solution, n, d, f)
