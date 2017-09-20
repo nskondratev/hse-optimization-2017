859fd7bde8a4ef3ad60e23e5e8f816fb5c07b37c
nskondratev <n.kondratyev@smartplayer.org>
2017-09-20 09:54:16 +0300

    Feature. Test util functions on small example.

--- lab_1/main.py
+++ lab_1/main.py
@@ -1,6 +1,10 @@
-import lab_1.input_provider as ip
+import lab_1.util as util
 
 if __name__ == '__main__':
-    test_filename = 'tai20a'
-    n, d, f = ip.read_from_file(test_filename)
-    print('Read from ip file: n = {}, d.shape = {}, f.shape = {}'.format(n, d.shape, f.shape))
+    test_filename = 'test3'
+    n, d, f = util.read_from_file(test_filename)
+    print('Read from file: n = {}, d.shape = {}, f.shape = {}'.format(n, d.shape, f.shape))
+    sol = util.build_initial_solution(n)
+    sol_obj = util.calc_obj_fun_value(sol, n, d, f)
+    print('Find initial solution: {}, objective func value = {}'.format(sol, sol_obj))
+
