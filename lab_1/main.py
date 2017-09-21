696aeb824ca520d40844c6d68025477f671985fe
Nikita Kondratev <highflyer16@yandex.ru>
2017-09-21 23:06:22 +0300

    Feature. Make use of iterated local search.

--- lab_1/main.py
+++ lab_1/main.py
@@ -2,9 +2,9 @@ import lab_1.util as util
 import lab_1.algorithms as alg
 
 if __name__ == '__main__':
-    test_filename = 'test3'
+    test_filename = 'tai12a'
     n, d, f = util.read_from_file(test_filename)
     print('Read from file: n = {}, d.shape = {}, f.shape = {}'.format(n, d.shape, f.shape))
-    sol, sol_obj = alg.two_opt(n, d, f)
+    sol, sol_obj = alg.iterated_local_search(n, d, f)
     print('Find solution: {}, objective func value = {}'.format(sol, sol_obj))
 
