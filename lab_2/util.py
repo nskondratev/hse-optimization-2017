caba33cd1076cfdce9e3b8cfcb38ba5bd33801dc
Nikita Kondratev <highflyer16@yandex.ru>
2017-11-04 12:04:15 +0300

    Feature. Implement logger config.

--- lab_2/util.py
+++ lab_2/util.py
@@ -1,8 +1,46 @@
 import os
 import numpy as np
 import random
+import logging
+import datetime as dt
 
 
+# Ensure if dir does exist
+def ensure_dir(dirpath):
+    if not os.path.exists(dirpath):
+        os.mkdir(dirpath)
+
+
+'''
+Start logger config
+'''
+LOGS_DIR = 'logs'
+LOG_FILENAME = 'run_{}.log'.format(dt.datetime.now().strftime('%Y_%m_%d_%H_%M_%S'))
+
+ensure_dir('logs')
+
+# Logger formatter
+log_formatter = logging.Formatter('%(asctime)s [%(filename)s:%(lineno)d] [%(levelname)s] %(message)s')
+logger = logging.getLogger()
+
+# Logger file handler
+file_handler = logging.FileHandler(os.path.join(LOGS_DIR, LOG_FILENAME))
+file_handler.setFormatter(log_formatter)
+logger.addHandler(file_handler)
+
+# Logger console handler
+console_handler = logging.StreamHandler()
+console_handler.setFormatter(log_formatter)
+logger.addHandler(console_handler)
+
+# Logger level
+logger.setLevel(os.environ.get('LOG_LEVEL') or logging.INFO)
+'''
+End logger config
+'''
+
+
+# Read input data from file
 def read_from_file(filepath):
     with open(filepath, 'r') as f:
         first_line = f.readline().strip().split(' ')
@@ -30,6 +68,7 @@ def objective_function(matrix, n1, clusters_row, clusters_col):
                     n_zeros_in += 1
 
     return n_ones_in / (n1 + n_zeros_in)
+
 
 def generate_initial_solution(m, p):
     a = random.randint(1, m - 1)
