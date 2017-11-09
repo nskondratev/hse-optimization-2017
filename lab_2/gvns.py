e0c5d36fe697369ff7e2927d5002c38f06892609
Nikita Kondratev <highflyer16@yandex.ru>
2017-11-09 08:38:44 +0300

    Fix. Handle None after shaking in GVNS.

--- lab_2/gvns.py
+++ lab_2/gvns.py
@@ -13,12 +13,12 @@ class GVNS:
                 while l < len(ls_neighborhood):
                     cur_ls_neighborhood = ls_neighborhood[l]
                     x2 = cur_ls_neighborhood.improve_solution(x1)
-                    if x2 is not None and x2.is_better(x1):
+                    if x2 is not None and (x1 is None or x2.is_better(x1)):
                         x1 = x2
                         l = 0
                     else:
                         l += 1
-                if x1.is_better(x):
+                if x1 is not None and x1.is_better(x):
                     x = x1
                     k = 0
                 else:
